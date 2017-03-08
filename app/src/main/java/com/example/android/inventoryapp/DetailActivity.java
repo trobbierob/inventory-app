a1841bdb830c748b363b2afed67035da890a7a38
trobbierob <troberts@my.hpu.edu>
2017-03-07 20:43:10 -0500

    Update UI
    Add icon to fab. Add delete button to main window.

--- app/src/main/java/com/example/android/inventoryapp/DetailActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/DetailActivity.java
@@ -1,4 +1,13 @@
 package com.example.android.inventoryapp;
 
-public class DetailActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
 }
--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -4,9 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+
+import com.example.android.inventoryapp.data.InventoryDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private InventoryDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ListView inListView = (ListView) findViewById(R.id.list);
+
+        View emptyView = findViewById(R.id.empty_view);
+        inListView.setEmptyView(emptyView);
+
+        mDbHelper = new InventoryDbHelper(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
