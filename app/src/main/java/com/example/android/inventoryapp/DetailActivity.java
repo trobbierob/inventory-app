b9517c6dda0fabca2624ab7728d9c21d077de0e0
trobbierob <troberts@my.hpu.edu>
2017-03-09 07:53:24 -0500

    Update Qty TextView into EditText

--- app/src/main/java/com/example/android/inventoryapp/DetailActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/DetailActivity.java
@@ -31,7 +31,7 @@ public class DetailActivity extends AppCompatActivity implements
     private static final int EXISTING_ITEM_LOADER = 0;
     private Uri mCurrentStockUri;
     private EditText mNameEditText;
-    private TextView mQuantityTextView;
+    private EditText mQuantityEditText;
     private EditText mPriceEditText;
     private EditText mEmailEditText;
     private EditText mPhoneEditText;
@@ -66,13 +66,14 @@ public class DetailActivity extends AppCompatActivity implements
         }
 
         mNameEditText = (EditText) findViewById(R.id.name_edit_text);
-        mQuantityTextView = (TextView) findViewById(R.id.quantity);
+        mQuantityEditText = (EditText) findViewById(R.id.quantity_edit_text);
         mPriceEditText = (EditText) findViewById(R.id.price_edit_text);
         mEmailEditText = (EditText) findViewById(R.id.email_edit_text);
         mPhoneEditText = (EditText) findViewById(R.id.phone_edit_text);
 
         mNameEditText.setOnTouchListener(mTouchListener);
         mPriceEditText.setOnTouchListener(mTouchListener);
+        mQuantityEditText.setOnTouchListener(mTouchListener);
         mEmailEditText.setOnTouchListener(mTouchListener);
         mPhoneEditText.setOnTouchListener(mTouchListener);
 
@@ -158,6 +159,7 @@ public class DetailActivity extends AppCompatActivity implements
 
         String nameString = mNameEditText.getText().toString().trim();
         String priceString = mPriceEditText.getText().toString().trim();
+        String qtyString = mQuantityEditText.getText().toString().trim();
         String emailString = mEmailEditText.getText().toString().trim();
         String phoneString = mPhoneEditText.getText().toString().trim();
 
@@ -169,7 +171,7 @@ public class DetailActivity extends AppCompatActivity implements
 
         ContentValues values = new ContentValues();
         values.put(InventoryEntry.COLUMN_ITEM_NAME, nameString);
-        values.put(InventoryEntry.COLUMN_ITEM_QTY, qty);
+        values.put(InventoryEntry.COLUMN_ITEM_QTY, qtyString);
         values.put(InventoryEntry.COLUMN_ITEM_PRICE, priceString);
         values.put(InventoryEntry.COLUMN_ITEM_EMAIL, emailString);
         values.put(InventoryEntry.COLUMN_ITEM_PHONE, phoneString);
@@ -227,8 +229,8 @@ public class DetailActivity extends AppCompatActivity implements
     }
 
     public void displayQuantity() {
-        TextView quantityView = (TextView) findViewById(R.id.quantity);
-        quantityView.setText(String.valueOf(qty));
+        EditText quantityViewEditText = (EditText) findViewById(R.id.quantity_edit_text);
+        quantityViewEditText.setText(String.valueOf(qty));
     }
 
     @Override
@@ -272,7 +274,7 @@ public class DetailActivity extends AppCompatActivity implements
             itemPhone = cursor.getString(phoneColumnIndex);
 
             mNameEditText.setText(itemName);
-            mQuantityTextView.setText(itemQty);
+            mQuantityEditText.setText(itemQty);
             mPriceEditText.setText(itemPrice);
             mEmailEditText.setText(itemEmail);
             mPhoneEditText.setText(itemPhone);
@@ -282,7 +284,7 @@ public class DetailActivity extends AppCompatActivity implements
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         mNameEditText.setText("");
-        mQuantityTextView.setText("");
+        mQuantityEditText.setText("");
         mPriceEditText.setText("");
         mEmailEditText.setText("");
         mPhoneEditText.setText("");
