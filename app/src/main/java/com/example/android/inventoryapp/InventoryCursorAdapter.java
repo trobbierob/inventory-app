ad40565b39c72f7f84862ca02694e438a83c96b1
trobbierob <troberts@my.hpu.edu>
2017-03-12 12:27:26 -0400

    Locate position of Sale button click

--- app/src/main/java/com/example/android/inventoryapp/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.inventoryapp;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
 public class InventoryCursorAdapter extends CursorAdapter {
 
     public static final String LOG_TAG = InventoryCursorAdapter.class.getSimpleName();
+    private Uri mCurrentStockUri;
 
     public InventoryCursorAdapter(Context context, Cursor cursor) {
         super(context, cursor, 0);
@@ -26,30 +28,39 @@ public class InventoryCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
 
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
         final TextView qtyTextView = (TextView) view.findViewById(R.id.qty);
         TextView priceTextView = (TextView) view.findViewById(R.id.price);
 
+        int idColumnIndex = cursor.getColumnIndex(InventoryEntry._ID);
         int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_NAME);
         int qtyColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QTY);
         int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_PRICE);
+        Log.v(LOG_TAG, "nameColumnIndex is: " + nameColumnIndex);
 
+        final int itemId = cursor.getInt(idColumnIndex);
+        Log.v(LOG_TAG, "itemId is: " + itemId);
         String itemName = cursor.getString(nameColumnIndex);
         final int itemQty = cursor.getInt(qtyColumnIndex);
         String itemPrice = cursor.getString(priceColumnIndex);
+        Log.v(LOG_TAG, "itemName is: " + itemName);
         Log.v(LOG_TAG, "itemQty is: " + itemQty);
 
         nameTextView.setText(itemName);
         qtyTextView.setText("" + itemQty);       /**/
         priceTextView.setText(itemPrice);
 
+        final int position = cursor.getPosition();
+
         Button saleButton = (Button) view.findViewById(R.id.sale);
         saleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v(LOG_TAG, "I'm in the button!!");
+                cursor.moveToPosition(position);
+                Log.v(LOG_TAG, "The position is: " + position);
+                Log.v(LOG_TAG, "itemId is: " + itemId);
             }
         });
     }
